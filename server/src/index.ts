722d725935fb9c6682026f0a7104052373add834
rosavskiy <a.rosavsky@gmail.com>
2026-01-26 09:52:35 +0400

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,8 @@ app.use(cors({
   credentials: true,
 }))
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(limiter)
 
 // Health check
